89264b75a41e5c86fe855ad4bde2039d6a47e6cb
Andrew Tishenko <webmazaretto@gmail.com>
2022-04-21 16:40:10 +0700

    Update front.js

--- resources/js/front.js
+++ resources/js/front.js
@@ -579,7 +579,7 @@ $(document).ready(function () {
     $('#ANKETA_CLONE_TRIGGER').click(e => {
         const CLONE_ID = 'cloning'
 
-        if(count_anketa+1 === 30) {
+        if(count_anketa+1 === 31) {
             swal.fire({
                 title: 'Нельзя добавлять более 30 осмотров',
                 icon: 'warning'
