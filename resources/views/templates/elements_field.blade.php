5c7a3d7af7246deae4253117087ba20a5f4de0b6
ellidey <ellidey00@gmail.com>
2022-08-12 16:52:30 +0700

    Fix company_name in filters by client

--- resources/views/templates/elements_field.blade.php
+++ resources/views/templates/elements_field.blade.php
@@ -57,6 +57,22 @@
             <button type="button" data-croppie-id="{{ $uniqueInputId }}" class="btn croppie-delete btn-sm btn-danger">Удалить фото</button>
         </div>
     @endif
+@elseif ($v['type'] === 'select' && auth()->user()->role == 12 && $k === 'company_name')
+    @php
+        $default_value = is_array($default_value) ? $default_value : explode(',', $default_value);
+        $key = isset($v['getFieldKey']) ? $v['getFieldKey'] : 'id';
+        $value = isset($v['getField']) ? $v['getField'] : 'name';
+    @endphp
+    <select
+        disabled
+        name="company_name"
+        class="filled-select2 filled-select"
+    >
+        <option selected value="{{ auth()->user()->company->name }}">
+            {{ auth()->user()->company->name }}
+        </option>
+    </select>
+
 @elseif ($v['type'] === 'select' && auth()->user()->role == 12 && $k === 'company_id')
     @php
         $default_value = is_array($default_value) ? $default_value : explode(',', $default_value);
