29e3c00f4fb6b177914b9c13510f976b78ac23cd
ellidey <ellidey00@gmail.com>
2022-08-18 21:16:00 +0700

    Fix soft deletes cars

--- app/Car.php
+++ app/Car.php
@@ -6,6 +6,8 @@ use Illuminate\Database\Eloquent\Model;
 
 class Car extends Model
 {
+    use \Illuminate\Database\Eloquent\SoftDeletes;
+
     public $fillable = [
         'hash_id',
         //'old_id',
