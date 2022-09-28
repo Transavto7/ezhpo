4009f6922754d5bfc6285a0d8bd002b73e53da71
ellidey <ellidey00@gmail.com>
2022-09-28 21:09:33 +0700

    Fix migration temp

--- database/migrations/2022_09_20_113852_change_structure_in_settings_table.php
+++ database/migrations/2022_09_20_113852_change_structure_in_settings_table.php
@@ -15,22 +15,22 @@ class ChangeStructureInSettingsTable extends Migration
      */
     public function up()
     {
-        $settings = Settings::query()->first();
+//        $settings = Settings::query()->first();
 
-        Schema::table('settings', function (Blueprint $table) {
-            $table->string('key')->nullable()->unique();
-            $table->text('value')->nullable();
-        });
-
-        $this->createSetting('logo', $settings->logo);
-        $this->createSetting('sms_api_key', $settings->sms_api_key);
-        $this->createSetting('sms_text_driver', $settings->sms_text_driver);
-        $this->createSetting('sms_text_car', $settings->sms_text_car);
-        $this->createSetting('sms_text_phone', $settings->sms_text_phone);
-        $this->createSetting('sms_text_default', $settings->sms_text_default);
-        $this->createSetting('id_auto', 0);
-        $this->createSetting('id_auto_required', 0);
-        $settings->delete();
+//        Schema::table('settings', function (Blueprint $table) {
+//            $table->string('key')->nullable()->unique();
+//            $table->text('value')->nullable();
+//        });
+//
+//        $this->createSetting('logo', $settings->logo);
+//        $this->createSetting('sms_api_key', $settings->sms_api_key);
+//        $this->createSetting('sms_text_driver', $settings->sms_text_driver);
+//        $this->createSetting('sms_text_car', $settings->sms_text_car);
+//        $this->createSetting('sms_text_phone', $settings->sms_text_phone);
+//        $this->createSetting('sms_text_default', $settings->sms_text_default);
+//        $this->createSetting('id_auto', 0);
+//        $this->createSetting('id_auto_required', 0);
+//        $settings->delete();
 
         Schema::table('settings', function (Blueprint $table) {
             $table->dropColumn('logo');
