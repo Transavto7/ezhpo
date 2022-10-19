173ba7eae5c307121f7d10d5582f1a868bb6f565
hewpew <charredtalk@gmail.com>
2022-10-19 18:53:06 +0500

    Fix: Вывод при отстранении по правам 2

--- app/Http/Controllers/ApiController.php
+++ app/Http/Controllers/ApiController.php
@@ -153,17 +153,20 @@ class ApiController extends Controller
                 return !in_array($item, $deleteImportantFields);
             });
 
-            if ($_model['model'] == Company::class){
-                if(!user()->access('companies_access_field_where_call')){   //'Кому отправлять СМС при отстранении'
-                    unset($fields['where_call']);
-                }
-                if(!user()->access('companies_access_field_where_call_name')){  //'Кому звонить при отстранении (имя, должность)
-                    unset($fields['where_call_name']);
-                }
-            }
 
             $fieldsValues = new IndexController();
             $fieldsValues = $fieldsValues->elements[$model]['fields'];
+
+            if ($_model['model'] == Company::class){
+                if(!user()->access('companies_access_field_where_call')){   //'Кому отправлять СМС при отстранении'
+                    unset($fields['where_call']);
+                    unset($fieldsValues['where_call']);
+                }
+                if(!user()->access('companies_access_field_where_call_name')){  //'Кому звонить при отстранении (имя, должность)
+                    unset($fields['where_call_name']);
+                    unset($fieldsValues['where_call_name']);
+                }
+            }
 
             $data = $data->where($prop, $val)->get($fields)->first();
 
