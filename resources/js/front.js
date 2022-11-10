63b06768b4021a53b246558766b5217bd5cef4cc
hewpew <charredtalk@gmail.com>
2022-11-10 17:58:04 +0500

    Edit-fix

--- resources/js/front.js
+++ resources/js/front.js
@@ -1171,12 +1171,12 @@ $(document).ready(function () {
     field.change(function(e, { selected }){
 
         if(selected === 'Абонентская плата без реестров'){
-            // field.closest('.modal-body').find('select[name=essence]').prop("disabled", false);
+            field.closest('.modal-body').find('select[name=essence]').prop("disabled", false);
 
             field.closest('.modal-body').find('select[name=type_anketa]').prop( "disabled", true);
             field.closest('.modal-body').find('select[name="type_view[]"]').prop( "disabled", true);
 
-            // field.closest('.modal-body').find('select[name=essence]').prop('required', true) // тип осмотра
+            field.closest('.modal-body').find('select[name=essence]').prop('required', true) // тип осмотра
             field.closest('.modal-body').find('select[name=type_anketa]').prop('required', false) // тип осмотра
             field.closest('.modal-body').find('select[name="type_view[]"]').prop('required', false) // Реестр
         }else{
