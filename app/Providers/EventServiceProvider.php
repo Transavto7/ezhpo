811b718a38d10638603be11418c0e606c43c532e
Ilya <seintte@gmail.com>
2023-02-13 16:10:38 +0700

    Убрал обсервер

--- app/Providers/EventServiceProvider.php
+++ app/Providers/EventServiceProvider.php
@@ -3,7 +3,6 @@
 namespace App\Providers;
 
 use App\Driver;
-use App\Observers\DriverCreatingObserver;
 use Illuminate\Auth\Events\Registered;
 use Illuminate\Auth\Listeners\SendEmailVerificationNotification;
 use Illuminate\Foundation\Support\Providers\EventServiceProvider as ServiceProvider;
@@ -29,6 +28,5 @@ class EventServiceProvider extends ServiceProvider
     public function boot()
     {
         parent::boot();
-        Driver::observe(DriverCreatingObserver::class);
     }
 }
