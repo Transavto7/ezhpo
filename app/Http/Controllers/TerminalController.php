e541dd3d39a98bd59b0287c45f5282b5a9a469fb
ellidey <ellidey00@gmail.com>
2023-02-20 18:03:16 +0700

    Change terminals and bdd

--- app/Http/Controllers/TerminalController.php
+++ app/Http/Controllers/TerminalController.php
@@ -102,6 +102,7 @@ class TerminalController extends Controller
                 'password' => $api_token,
                 'api_token' => $api_token,
                 'email' => time() . '@ta-7.ru',
+                'login' => time() . '@ta-7.ru',
                 'pv_id' => $request->get('pv', null),
             ]);
         }
