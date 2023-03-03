940f7eece0912ed0483bf77d9d85d4bbf21360e2
robotateme <jarwork884@gmail.com>
2023-03-03 14:00:09 +0300

    Левое меню изменение элементов

--- resources/js/components/admin/sidebar/AdminSidebarItemModal.vue
+++ resources/js/components/admin/sidebar/AdminSidebarItemModal.vue
@@ -13,6 +13,14 @@
         />
       </div>
       <div class="form-group mb-3">
+        <label class="mb-1">Короткое название-идентификатор</label>
+        <b-form-input
+            v-model="item.slug"
+            size="sm"
+            placeholder="Введите название"
+        />
+      </div>
+      <div class="form-group mb-3">
         <label class="mb-1">Родительский элемент меню</label>
         <b-form-select
             v-model="item.parent_id"
