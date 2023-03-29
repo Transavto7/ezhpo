7c0af3ef6605b90875af1fdf8799037d2c167638
robotateme <jarwork884@gmail.com>
2023-03-29 09:44:55 +0300

    Blood pressure fixed

--- app/helpers.php
+++ app/helpers.php
@@ -22,7 +22,7 @@ if (!function_exists('subtract_array_values')) {
 if (!function_exists('in_array_thresholds')) {
     function in_array_thresholds(int $value, array $thresholds): bool
     {
-        return ($value >= min($thresholds)) && ($value <= max($thresholds));
+        return (($value >= min($thresholds)) && ($value <= max($thresholds)));
     }
 }
 
@@ -30,5 +30,13 @@ if (!function_exists('phone_sanitizing')) {
     function phone_sanitizing(string $phone) : string
     {
         return preg_replace('/^0|[^а-яА-Я0-9+]+/u', '', filter_var($phone, FILTER_SANITIZE_NUMBER_INT));
+    }
+}
+
+if (!function_exists('tonometer_sanitizing')) {
+    function tonometer_sanitizing(string $tonometer) : string
+    {
+        $tonometer = str_replace(['//', "\\"], ['/', '/'], $tonometer);
+        return preg_replace('/[^0-9\/]/',"", $tonometer);
     }
 }
