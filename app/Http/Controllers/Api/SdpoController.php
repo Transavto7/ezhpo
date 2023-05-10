36d3d47572dedc874184c21d27a1cd41ad32f458
flaim61 <flaim611@gmail.com>
2023-05-10 15:54:15 +0600

    Update SdpoController.php

--- app/Http/Controllers/Api/SdpoController.php
+++ app/Http/Controllers/Api/SdpoController.php
@@ -150,7 +150,7 @@ class SdpoController extends Controller
             }
 
             if ($proba_alko === 'Положительно') {
-                $driver->end_of_ban = Carbon::now("GMT")->addMinutes($driver->getTimeOfAlcoholBan());
+                $driver->end_of_ban = Carbon::now("GMT")->addMinutes(120);
             }
         }
 
