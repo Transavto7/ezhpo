9596dd29dcd30f8a7451731c174f3ebec68af84e
Angryogurt <timondimon98@gmail.com>
2023-12-14 01:47:49 +0300

    fix(SMS): отправка отключена, если приложение не в production

--- app/Http/Controllers/SmsController.php
+++ app/Http/Controllers/SmsController.php
@@ -3,19 +3,18 @@
 namespace App\Http\Controllers;
 
 use App\Settings;
-use Illuminate\Http\Request;
 
 class SmsController extends Controller
 {
     public function sms ($to, $msg) {
+        if (config('app.env') !== 'production') return '';
+
         $api_key = Settings::setting('sms_api_key');
 
         if($api_key) {
             $body = file_get_contents("https://sms.ru/sms/send?api_id=$api_key&to=$to&msg=".urlencode($msg)."&json=1");
 
-            $json = json_decode($body);
-
-            return $json;
+            return json_decode($body);
         }
 
         return '';
