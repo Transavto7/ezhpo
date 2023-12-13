dedc7ed11b2424a2db2b95689c9cdd08d249e4e1
Angryogurt <timondimon98@gmail.com>
2023-12-14 01:49:20 +0300

    fix(Medic): фикс ошибки в VO температуры

--- app/ValueObjects/Temperature.php
+++ app/ValueObjects/Temperature.php
@@ -16,14 +16,7 @@ class Temperature
 
     public static function random(): self
     {
-        function mt_rand_float($min, $max, $countZero = '0') {
-            $countZero = +('1'.$countZero);
-            $min = floor($min * $countZero);
-            $max = floor($max * $countZero);
-            return mt_rand($min, $max) / $countZero;
-        }
-
-        return new self(mt_rand_float(35.9,36.7));
+        return new self(mt_rand(35.9, 36.7));
     }
 
     public function isAdmitted(): bool
