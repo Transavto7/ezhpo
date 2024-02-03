5da83cb16af25dd664de435b4635540599c956a7
Angryogurt <timondimon98@gmail.com>
2024-02-03 23:08:06 +0300

    feat(Cars): типы т\с
    - обновлены 4 типа т\с
    - добавлены 4 новых
    - название колонки в фильтрах изменено

---
+++ database/migrations/2024_02_03_223811_update_car_types_enum.php
@@ -0,0 +1,60 @@
+<?php
+
+use App\Car;
+use App\FieldPrompt;
+use Illuminate\Database\Migrations\Migration;
+
+class UpdateCarTypesEnum extends Migration
+{
+    protected $enumElements = [
+        'В и грузовые автомобили до 3.5 т.' => 'В - легковые и грузовые автомобили до 3,5 тн',
+        'С (свыше 3.5 т.)' => 'С - Грузовые т/с от 3,5 тн',
+        'D' => 'D - автобусы',
+        'E' => 'E',
+    ];
+
+    protected $title = [
+        'old' => 'Тип автомобиля',
+        'new' => 'Тип т\с'
+    ];
+
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        FieldPrompt::query()
+            ->where('type', 'car')
+            ->where('name', $this->title['old'])
+            ->update(['name' => $this->title['new']]);
+
+        foreach ($this->enumElements as $oldValue => $newValue) {
+            Car::query()
+                ->withTrashed()
+                ->where('type_auto', $oldValue)
+                ->update(['type_auto' => $newValue]);
+        }
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        FieldPrompt::query()
+            ->where('type', 'car')
+            ->where('name', $this->title['new'])
+            ->update(['name' => $this->title['old']]);
+
+        foreach ($this->enumElements as $oldValue => $newValue) {
+            Car::query()
+                ->withTrashed()
+                ->where('type_auto', $newValue)
+                ->update(['type_auto' => $oldValue]);
+        }
+    }
+}
