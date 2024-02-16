7edb809292ae0d2e56f0d190558b86fb679ca153
Angryogurt <timondimon98@gmail.com>
2024-02-16 18:28:16 +0300

    hotfix(Elements): логирование получения инпута без пользователя

--- resources/views/templates/elements_field.blade.php
+++ resources/views/templates/elements_field.blade.php
@@ -2,6 +2,23 @@
     $default_value = $default_value ?? '';
     /** @var \App\User $user */
     $user = \Illuminate\Support\Facades\Auth::user();
+
+    if (empty($user)) {
+        $request = request();
+
+        \Illuminate\Support\Facades\Log::channel('deprecated-api')->info(json_encode(
+                [
+                    'request' => $request->all(),
+                    'headers' => $request->headers->all(),
+                    'user-web' => \Illuminate\Support\Facades\Auth::guard('web')->user(),
+                    'user-api' => \Illuminate\Support\Facades\Auth::guard('api')->user(),
+                    'user' => \Illuminate\Support\Facades\Auth::user(),
+                    'url' => $request->url(),
+                    'full-url' => $request->fullUrl(),
+                    'ip' => $request->getClientIp() ?? null,
+                ]
+            ));
+    }
 @endphp
 
 @if($v['type'] !== 'select')
