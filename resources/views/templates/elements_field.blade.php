43d33f55487ec4cf4822cd14295dfb4d5580c0b9
Angryogurt <timondimon98@gmail.com>
2024-02-19 22:19:40 +0300

    hotfix(Elements): получение пользователя для получения инпута

--- resources/views/templates/elements_field.blade.php
+++ resources/views/templates/elements_field.blade.php
@@ -1,7 +1,8 @@
 @php
     $default_value = $default_value ?? '';
     /** @var \App\User $user */
-    $user = \Illuminate\Support\Facades\Auth::user();
+    $user = \Illuminate\Support\Facades\Auth::guard("web")->user() ??
+        \Illuminate\Support\Facades\Auth::guard("api")->user();
 
     if (empty($user)) {
         $request = request();
@@ -10,9 +11,6 @@
                 [
                     'request' => $request->all(),
                     'headers' => $request->headers->all(),
-                    'user-web' => \Illuminate\Support\Facades\Auth::guard('web')->user(),
-                    'user-api' => \Illuminate\Support\Facades\Auth::guard('api')->user(),
-                    'user' => \Illuminate\Support\Facades\Auth::user(),
                     'url' => $request->url(),
                     'full-url' => $request->fullUrl(),
                     'ip' => $request->getClientIp() ?? null,
