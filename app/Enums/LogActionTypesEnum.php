534d1016d8629270395daffbdcc31559ab12e029
Angryogurt <timondimon98@gmail.com>
2024-05-20 23:29:56 +0300

    feat(Logs): енумы действий и контекстов

---
+++ app/Enums/LogActionTypesEnum.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Enums;
+
+use Illuminate\Support\Collection;
+
+class LogActionTypesEnum
+{
+    const CREATING = 'creating';
+    const UPDATING = 'updating';
+    const DELETING = 'deleting';
+    const RESTORING = 'restoring';
+
+    public static function labels(): array
+    {
+        return [
+            self::CREATING => 'Создание',
+            self::UPDATING => 'Редактирование',
+            self::DELETING => 'Удаление',
+            self::RESTORING => 'Восстановление'
+        ];
+    }
+
+    public static function options(): Collection
+    {
+        return collect(self::labels())
+            ->map(function ($value, $key) {
+                return [
+                    'id' => $key,
+                    'text' => $value
+                ];
+            })
+            ->values();
+    }
+
+    public static function label(string $value): string
+    {
+        return self::labels()[$value] ?? 'Неизвестное действие';
+    }
+}
