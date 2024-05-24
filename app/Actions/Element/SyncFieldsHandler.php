f6ebccc66689d375c3451c79d673dbeb50e532c3
Angryogurt <timondimon98@gmail.com>
2024-05-24 09:54:11 +0300

    feat(Logs): логирование массовых обновлений полей

--- app/Actions/Element/SyncFieldsHandler.php
+++ app/Actions/Element/SyncFieldsHandler.php
@@ -2,6 +2,10 @@
 
 namespace App\Actions\Element;
 
+use App\Enums\LogActionTypesEnum;
+use App\Log;
+use Illuminate\Support\Facades\Auth;
+
 class SyncFieldsHandler
 {
     public function handle(array $data): int
@@ -18,6 +22,41 @@ class SyncFieldsHandler
             $query->where('autosync_fields', 'LIKE', "%$data[fieldSync]%");
         }
 
-        return $query->update([$data['fieldSync'] => $data['fieldSyncValue']]);
+        $newValue = $data['fieldSyncValue'];
+        $field = $data['fieldSync'];
+        //TODO: здесь некорректно брать пользователя, переместить выше
+        $userId = Auth::id();
+
+        $query
+            ->select([
+                'id',
+                $field
+            ])
+            ->get()
+            ->each(function ($model) use ($newValue, $field, $userId) {
+                $oldValue = $model[$field];
+
+                if ($oldValue == $newValue) return;
+
+                /** @var Log $log */
+                $log = Log::create([
+                    'user_id' => $userId,
+                    'type' => LogActionTypesEnum::UPDATING
+                ]);
+
+                $log->setAttribute('data', [
+                    [
+                        'name' => $field,
+                        'oldValue' => $oldValue,
+                        'newValue' => $newValue
+                    ]
+                ]);
+
+                $log->model()->associate($model);
+
+                $log->save();
+            });
+
+        return $query->update([$field => $newValue]);
     }
 }
