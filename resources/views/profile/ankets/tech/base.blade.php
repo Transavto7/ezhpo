f75a26bec0d2d5adf540a44cc35b5191c2ff9535
Angryogurt <timondimon98@gmail.com>
2024-07-30 22:46:23 +0300

    refactor(Forms): уменьшение размера форм (WIP)
    - фикс перепутанных местами блейдов Ввод ПЛ и обычного ТО

--- resources/views/profile/ankets/tech/base.blade.php
+++ resources/views/profile/ankets/tech/base.blade.php
@@ -1,23 +1,8 @@
 <div class="form-group">
-    <label class="form-control-label">ID компании:</label>
-    <article>
-        <input value="{{ $company_id ?? '' }}"
-               required
-               type="number"
-               oninput="if(this.value.length >= 0) checkInputProp('hash_id', 'Company', event.target.value, 'name', $(event.target).parent(), {{ !($id ?? false) ? 'true' : 'false' }})"
-               min="5"
-               name="company_id"
-               class="MASK_ID_ELEM form-control">
-        <p class="app-checker-prop"></p>
-    </article>
-</div>
-
-<div class="form-group">
     <label class="form-control-label">ID водителя:</label>
     <article>
-        <input value="{{ $driver_id ?? '' }}"
-               type="number"
-               oninput="if(this.value.length >= 0) checkInputProp('hash_id', 'Driver', event.target.value, 'fio', $(event.target).parent(), {{ !($id ?? false) ? 'true' : 'false' }})"
+        <input required value="{{ $driver_id ?? '' }}" type="number"
+               oninput="if(this.value.length >= 0) checkInputProp('hash_id', 'Driver', event.target.value, 'fio', $(event.target).parent(), {{ 'false' }})"
                min="6"
                name="driver_id"
                class="MASK_ID_ELEM form-control">
@@ -27,11 +12,22 @@
 
 <div class="cloning" id="cloning-first">
     <div class="form-group">
+        <label class="form-control-label">Дата и время осмотра:</label>
+        <article>
+            <input min="1900-02-20T20:20"
+                   max="2999-02-20T20:20"
+                   type="datetime-local"
+                   value="{{ $default_current_date ?? '' }}"
+                   name="anketa[0][date]"
+                   class="form-control">
+        </article>
+    </div>
+
+    <div class="form-group">
         <label class="form-control-label">ID автомобиля:</label>
         <article>
-            <input value="{{ $car_id ?? '' }}"
-                   type="number"
-                   oninput="if(this.value.length >= 0) checkInputProp('hash_id', 'Car', event.target.value, 'gos_number', $(event.target).parent(), {{ !($id ?? false) ? 'true' : 'false' }})"
+            <input required value="{{ $car_id ?? '' }}" type="number"
+                   oninput="if(this.value.length >= 0) checkInputProp('hash_id', 'Car', event.target.value, 'gos_number', $(event.target).parent(), {{ 'false' }})"
                    min="6"
                    name="anketa[0][car_id]"
                    class="MASK_ID_ELEM form-control car-input">
@@ -40,56 +36,9 @@
     </div>
 
     <div class="form-group">
-        <label class="form-control-label">Дата осмотра ПЛ:</label>
-        <article>
-            <input min="1900-02-20T20:20"
-                   max="2999-02-20T20:20"
-                   type="datetime-local"
-                   @if (!isset($period_pl)) required @endif
-                   oninput="changeFormRequire(this, 'pl-period')"
-                   @isset ($date) value="{{ $default_current_date ?? '' }}" @endisset
-                   name="anketa[0][date]"
-                   class="form-control pl-date">
-        </article>
-    </div>
-
-    @if(empty($id))
-        <div class="form-group">
-            <label class="form-control-label">Доп. даты:</label>
-            <article>
-                <input type="date"
-                       name="anketa[0][dates]"
-                       class="form-control date-range">
-            </article>
-        </div>
-    @endif
-
-    <div class="form-group">
-        <label class="form-control-label">Период выдачи ПЛ:</label>
-        <article>
-            <input type="month"
-                   @if (!isset($date)) required @endif
-                   oninput="changeFormRequire(this, 'pl-date')"
-                   value="{{ isset($period_pl) ? $period_pl : '' }}"
-                   name="anketa[0][period_pl]"
-                   class="form-control pl-period">
-        </article>
-    </div>
-
-    <div class="form-group">
-        <label class="form-control-label">Номер путевого листа:</label>
-        <article>
-            <input type="text"
-                   value="{{ $number_list_road ?? '' }}"
-                   name="anketa[0][number_list_road]"
-                   class="form-control">
-        </article>
-    </div>
-
-    <div class="form-group">
         <label class="form-control-label">Тип осмотра:</label>
         <article>
-            <select name="anketa[0][type_view]" class="form-control">
+            <select name="anketa[0][type_view]" required class="form-control">
                 <option value="Предрейсовый/Предсменный"
                         @if(strcasecmp($type_view ?? '', 'Предрейсовый/Предсменный') == 0) selected @endif>
                     Предрейсовый/Предсменный
@@ -122,13 +71,30 @@
 
     <div class="form-group">
         <label class="form-control-label">Показания одометра:</label>
-        <article>
-            <input type="text"
-                   value="{{ $odometer ?? '' }}"
-                   name="anketa[0][odometer]"
-                   class="form-control">
-        </article>
+        <input type="text" value="{{ $odometer ?? '' }}"
+               name="anketa[0][odometer]"
+               class="form-control">
+    </div>
+
+    <div class="form-group">
+        <label class="form-control-label">Номер путевого листа:</label>
+        <input type="text" value="{{ $number_list_road ?? '' }}"
+               name="anketa[0][number_list_road]"
+               class="form-control">
     </div>
 
     <div class="anketa-delete"></div>
 </div>
+
+<div class="form-group">
+    <label class="form-control-label">Отметка о прохождении предрейсового контроля:</label>
+    <select name="point_reys_control" id="point_reys_control" required class="form-control">
+        @isset($point_reys_control)
+            <option disabled selected value="{{ $point_reys_control }}">{{ $point_reys_control }}</option>
+        @endisset
+
+        <option selected value="Пройден">Пройден</option>
+        <option value="Не пройден">Не пройден</option>
+    </select>
+</div>
+
