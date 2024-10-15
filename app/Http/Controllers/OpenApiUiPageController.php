c2ad1a3b379dd5697de4ddc230a32b742eea0417
heavenlybilly <heavenlybilly@gmail.com>
2024-10-15 22:29:07 +0300

    feat: добавление разрешения на просмотр OpenApi UI

--- app/Http/Controllers/OpenApiUiPageController.php
+++ app/Http/Controllers/OpenApiUiPageController.php
@@ -3,12 +3,17 @@
 namespace App\Http\Controllers;
 
 use Auth;
+use Symfony\Component\HttpFoundation\Response;
 
 class OpenApiUiPageController extends Controller
 {
     public function index()
     {
         $user = Auth::user();
+
+        if (!$user->access('openapi_read')) {
+            abort(Response::HTTP_FORBIDDEN);
+        }
 
         return view('openapi', [
             'apiToken' => $user->api_token,
