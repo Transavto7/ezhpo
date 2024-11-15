b81476f7163ecfa6ac5bd7892e9885a4134c25fc
Angryogurt <timondimon98@gmail.com>
2024-11-15 03:39:03 +0300

    fix: удаление FK в таблице с корректным значением давления
    - добавлено удаление строк без связи с существующими записями в таблице anketas\forms
    - добавлена проверка существования FK

--- database/migrations/2024_11_11_152011_change_fk_in_medic_form_normalized_pressures_table.php
+++ database/migrations/2024_11_11_152011_change_fk_in_medic_form_normalized_pressures_table.php
@@ -1,11 +1,15 @@
 <?php
 
+use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
 class ChangeFkInMedicFormNormalizedPressuresTable extends Migration
 {
+    private static $TABLE = 'medic_form_normalized_pressures';
+
+    private static $KEY = 'medic_form_normalized_pressures_form_id_foreign';
     /**
      * Run the migrations.
      *
@@ -13,8 +17,17 @@ class ChangeFkInMedicFormNormalizedPressuresTable extends Migration
      */
     public function up()
     {
-        Schema::table('medic_form_normalized_pressures', function (Blueprint $table) {
-            $table->dropForeign(['form_id']);
+        Schema::table(self::$TABLE, function (Blueprint $table) {
+            $foreignKeys = $this->listTableForeignKeys(self::$TABLE);
+            if (in_array(self::$KEY, $foreignKeys)) {
+                $table->dropForeign(self::$KEY);
+            }
+
+            DB::table(self::$TABLE)
+                ->leftJoin('forms', 'forms.id', '=', self::$TABLE . '.form_id')
+                ->whereNull('forms.id')
+                ->delete();
+
             $table->foreign('form_id')
                 ->references('id')
                 ->on('forms')
@@ -29,12 +42,30 @@ class ChangeFkInMedicFormNormalizedPressuresTable extends Migration
      */
     public function down()
     {
-        Schema::table('medic_form_normalized_pressures', function (Blueprint $table) {
-            $table->dropForeign(['form_id']);
+        Schema::table(self::$TABLE, function (Blueprint $table) {
+            $foreignKeys = $this->listTableForeignKeys(self::$TABLE);
+            if (in_array(self::$KEY, $foreignKeys)) {
+                $table->dropForeign(self::$KEY);
+            }
+
+            DB::table(self::$TABLE)
+                ->leftJoin('anketas', 'anketas.id', '=', self::$TABLE . '.form_id')
+                ->whereNull('anketas.id')
+                ->delete();
+
             $table->foreign('form_id')
                 ->references('id')
                 ->on('anketas')
                 ->onDelete('cascade');
         });
+    }
+
+    private function listTableForeignKeys($table)
+    {
+        $conn = Schema::getConnection()->getDoctrineSchemaManager();
+
+        return array_map(function($key) {
+            return $key->getName();
+        }, $conn->listTableForeignKeys($table));
     }
 }
