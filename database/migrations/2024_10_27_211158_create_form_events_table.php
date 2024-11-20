0cfeb1c2b6a35a4d274114e7f4bf6d803369a076
ogsolo <kortik1999@yandex.ru>
2024-11-20 23:25:20 +0300

    feat: объединение в одну таблицу логов осмотров и события обратной связи

--- database/migrations/2024_10_27_211158_create_form_events_table.php
+++ database/migrations/2024_10_27_211158_create_form_events_table.php
@@ -21,10 +21,11 @@ class CreateFormEventsTable extends Migration
             $table->foreign('form_uuid')->references('uuid')->on('forms');
 
             $table->string('event_type');
-            $table->json('payload');
+            $table->jsonb('payload');
 
             $table->bigInteger('user_id')->unsigned();
             $table->foreign('user_id')->references('id')->on('users');
+            $table->string('model_type')->nullable();
 
             $table->timestamps();
         });
