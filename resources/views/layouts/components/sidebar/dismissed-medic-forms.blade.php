b4418c423a7379ce670023c2dc492229b6ec632a
angryogurt <timondimon98@gmail.com>
2025-03-28 01:38:02 +0300

    feat: кнопка быстрого просмотра отстранений МО

---
+++ resources/views/layouts/components/sidebar/dismissed-medic-forms.blade.php
@@ -0,0 +1,22 @@
+@php
+    use App\Enums\FormTypeEnum;
+    use Illuminate\Support\Arr;
+    use Illuminate\Support\Facades\Auth;
+    use App\User;
+
+    /** @var User|null $user */
+    $user = Auth::user();
+
+    $dismissedMedicFormsUrl = route('home', FormTypeEnum::MEDIC) . "?" . Arr::query([
+        'filter' => 1,
+        'date' => now()->subMonth()->startOfMonth()->format('Y-m-d'),
+        'TO_date' => now()->subMonth()->endOfMonth()->format('Y-m-d'),
+        'admitted' => ['Не допущен', 'Не идентифицирован'],
+        'company_id' => ($user && $user->hasRole('client') && $user->company) ? $user->company->hash_id : null
+    ]);
+@endphp
+<li>
+    <a href="{{ $dismissedMedicFormsUrl }}" class="bg-success text-white">
+        <i class="fa fa-book"></i>Отстранения водителей
+    </a>
+</li>
