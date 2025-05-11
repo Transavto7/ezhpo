8b92641b6e5fda82b3388874fa17ca75b429e725
heavenlybilly <heavenlybilly@gmail.com>
2025-05-12 01:30:37 +0300

    fix: команда обновления created_at осмотров

--- app/Console/Commands/SplitUsers/FixCreatedAtInFormsCommand.php
+++ app/Console/Commands/SplitUsers/FixCreatedAtInFormsCommand.php
@@ -2,10 +2,13 @@
 
 namespace App\Console\Commands\SplitUsers;
 
+use App\Enums\FlagPakEnum;
 use App\Enums\FormLogActionTypesEnum;
+use App\Enums\FormTypeEnum;
 use App\Events\Forms\FormAction;
 use App\Models\Forms\Form;
 use App\User;
+use Carbon\Carbon;
 use Illuminate\Console\Command;
 use Illuminate\Support\Facades\DB;
 
@@ -20,7 +23,7 @@ final class FixCreatedAtInFormsCommand extends Command
      *
      * @var string
      */
-    protected $signature = 'split-users:export-forms-with-wrong-created-at';
+    protected $signature = 'split-users:fix-medic-forms-created-at';
 
     /**
      * The console command description.
@@ -31,6 +34,7 @@ final class FixCreatedAtInFormsCommand extends Command
 
     public function handle()
     {
+        $logData = [];
         $user = User::where('login', '=', User::DEFAULT_USER_LOGIN)->first();
 
         if (! $user) {
@@ -40,16 +44,35 @@ final class FixCreatedAtInFormsCommand extends Command
         DB::beginTransaction();
 
         Form::withTrashed()
+            ->select([
+                'forms.*',
+            ])
             ->leftJoin('employees', 'employees.related_user_id', '=', 'forms.user_id')
+            ->leftJoin('medic_forms', 'medic_forms.forms_uuid', '=', 'forms.uuid')
             ->where('forms.created_at', '>=', self::UPDATE_DATE)
             ->where('forms.created_at', '<', self::FIX_DATE)
             ->where('forms.updated_at', '>=', self::UPDATE_DATE)
             ->whereNull('employees.timezone')
             ->whereNotNull('employees.id')
-            ->each(function (Form $form) use ($user) {
+            ->where(function ($query) {
+                $query->where('forms.type_anketa', '!=', FormTypeEnum::MEDIC)
+                    ->orWhere('medic_forms.flag_pak', '=', FlagPakEnum::INTERNAL);
+            })
+            ->orderBy('forms.created_at')
+            ->each(function (Form $form) use ($user, &$logData) {
                 try {
+                    $oldValue = $form->created_at;
+                    $newValue = $oldValue->copy()->addHours(3);
+
                     $form->created_at = $form->created_at->copy()->addHours(3);
                     event(new FormAction($user, $form, FormLogActionTypesEnum::CORRECTION));
+
+                    $this->comment("Обновление created_at осмотра [$form->id]: $oldValue -> $newValue");
+                    $logData[] = [
+                        'id' => $form->id,
+                        'old_created_at' => $oldValue->format('Y-m-d H:i:s'),
+                        'new_created_at' => $newValue->format('Y-m-d H:i:s'),
+                    ];
 
                     $form->save();
                 } catch (\Exception $exception) {
@@ -60,6 +83,13 @@ final class FixCreatedAtInFormsCommand extends Command
             });
 
         DB::commit();
+
+        $json = json_encode($logData, JSON_PRETTY_PRINT | JSON_UNESCAPED_UNICODE);
+        $fileName = 'form_created_at_fix_'.Carbon::now()->format('Y-m-d_H-i-s').'.json';
+
+        file_put_contents(storage_path("app/$fileName"), $json);
+
+        $this->comment("\nОбновлено осмотров: ".count($logData)."\n");
         $this->info('Успешно завершено');
     }
 }
