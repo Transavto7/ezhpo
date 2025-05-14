52df68b8a17f7dd7b61b716aa3b79d93bc7a9976
danilin <example@gmail.com>
2025-05-14 09:19:06 +0300

    hotfix: обновление компании
    - устранена ошибка мержа

--- app/Actions/Element/Update/UpdateCompanyHandler.php
+++ app/Actions/Element/Update/UpdateCompanyHandler.php
@@ -62,6 +62,7 @@ class UpdateCompanyHandler extends UpdateElementHandler
      */
     protected function validateData()
     {
+        /** @var Company $existItem */
         $existItem = Company::query()
             ->where('id', '!=', $this->element->id)
             ->where('name', trim($this->data['name'] ?? ''))
@@ -70,14 +71,14 @@ class UpdateCompanyHandler extends UpdateElementHandler
             throw new EntityAlreadyExistException('Найден дубликат по названию компании');
         }
 
-        $this->validateReqs();
+        $this->validateReqs($existItem);
         $this->validatePhoneNumber();
     }
 
     /**
      * @throws Exception
      */
-    protected function validateReqs($id, Company $company)
+    protected function validateReqs(Company $company)
     {
         $reqsValidated = $company->getAttribute('reqs_validated');
 
@@ -123,7 +124,7 @@ class UpdateCompanyHandler extends UpdateElementHandler
             }
         }
 
-        $existItem = $this->companyRepository->findByReqs($companyReqs, $id);
+        $existItem = $this->companyRepository->findByReqs($companyReqs, $company->getAttribute('id'));
         if ($existItem) {
             throw new EntityAlreadyExistException('Найден дубликат компании по ИНН (+КПП)');
         }
