369de2d3e95369c374313586f0c7397c4f4b9810
ogsolo <kortik1999@yandex.ru>
2025-06-01 13:28:05 +0300

    feat: вывод фио и госномера на qr-коде

--- app/Services/QRCode/QRCodeStickerGenerator.php
+++ app/Services/QRCode/QRCodeStickerGenerator.php
@@ -2,6 +2,8 @@
 
 namespace App\Services\QRCode;
 
+use App\Car;
+use App\Driver;
 use App\Enums\QRCodeLinkParameter;
 use Barryvdh\DomPDF\Facade as PDF;
 use Illuminate\Contracts\Container\BindingResolutionException;
@@ -34,17 +36,19 @@ class QRCodeStickerGenerator
      */
     public function getPdfResponse()
     {
-        $customPaper = array(0,0,282.00,222.00);
-
+        $title = $this->getEntityTitle();
         $link = $this->linkGenerator->generate();
         $qrCode = $this->qrCodeGenerator->generate($link);
+        $type = $this->linkGenerator->getParameter()->value() === QRCodeLinkParameter::CAR_ID
+            ? self::CAR
+            : self::DRIVER;
+        $customPaper = [0, 0, $type === 'D' ? 375.00 : 335.00, 222.00];
 
         $pdf = Pdf::loadView('templates.qr-code', [
                 'qrCode' => $qrCode,
+                'title' => $title,
                 'id' => $this->linkGenerator->getId(),
-                'type' => $this->linkGenerator->getParameter()->value() === QRCodeLinkParameter::CAR_ID
-                    ? self::CAR
-                    : self::DRIVER,
+                'type' => $type,
                 'domain' => $this->getUrl()
             ])
             ->setPaper($customPaper, 'landscape');
@@ -70,6 +74,17 @@ class QRCodeStickerGenerator
         }
 
         return $url;
+    }
+
+    private function getEntityTitle(): string
+    {
+        if ($this->linkGenerator->getParameter()->value() === QRCodeLinkParameter::DRIVER_ID) {
+            return Driver::where('hash_id', $this->linkGenerator->getId())->first()->fio;
+        } elseif ($this->linkGenerator->getParameter()->value() === QRCodeLinkParameter::CAR_ID) {
+            return Car::where('hash_id', $this->linkGenerator->getId())->first()->gos_number;
+        } else {
+            throw new \DomainException('Тип сущности не указан или указан неверно. Тип: '.$this->linkGenerator->getParameter()->value(), 400);
+        }
     }
 
     public function getView()
--- resources/views/templates/qr-code.blade.php
+++ resources/views/templates/qr-code.blade.php
@@ -1,9 +1,42 @@
-<style>
-    @page { margin: 8px; }
-    body { margin: 8px; }
-</style>
+<!DOCTYPE html>
+<html>
+<head>
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+    <style>
+        body {
+            font-family: 'DejaVu Sans', sans-serif;
+            margin: 8px 4px 4px 8px;
+        }
+        @page { margin: 8px 4px 4px 8px; }
+        .content-block {
+            margin: 0 auto;
+            font-weight: bold;
+            text-align: center;
+        }
+        .title {
+            font-size: @if($type === 'D') 18pt @else 26pt @endif;
+            line-height: 1.1;
+            margin-bottom: 5px;
+        }
+        .domain {
+            font-size: 18pt;
+            line-height: 1.2;
+            margin: 5px auto 0;
+        }
+        .id-type {
+            font-size: 26pt;
+            line-height: 1.1;
+            margin: 2px auto 0;
+        }
+    </style>
+    <title></title>
+</head>
+<body>
 <div>
-    <img src="{{ $qrCode }}" alt="QR Code" style="text-align:center;"/>
-    <p style="margin: 10px auto 0; font-weight: bold; font-size: 18pt; text-align:center;">{{ $domain }}</p>
-    <p style="margin: 0 auto 0; font-weight: bold; font-size: 26pt; text-align:center;">{{ $id.' - '.$type }}</p>
+    <div class="content-block title">{{ $title }}</div>
+    <img src="{{ $qrCode }}" alt="QR Code" style="display: block; margin: 0 auto;"/>
+    <div class="content-block domain">{{ $domain }}</div>
+    <div class="content-block id-type">{{ $id.' - '.$type }}</div>
 </div>
+</body>
+</html>
